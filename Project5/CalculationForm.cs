63d3d4ced184890993b7a1f0ac9d44fac9f8f1f5
Alexa <Alexa@DESKTOP-RO67FS2>
2023-06-19 22:00:55 +0300

    Добавьте файлы проекта.

---
+++ Project5/CalculationForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Project5
+{
+    public partial class CalculationForm : Form
+    {
+        public CalculationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectedOperation = comboBox1.SelectedItem.ToString();
+
+            switch (selectedOperation)
+            {
+                case "Сума чисел":
+                    int sum = 0;
+                    foreach (char digitChar in textBox1.Text)
+                    {
+                        if (char.IsDigit(digitChar))
+                        {
+                            int digit = int.Parse(digitChar.ToString());
+                            sum += digit;
+                        }
+                    }
+                    label3.Text = sum.ToString();
+                    break;
+                case "Середнє арифметичне":
+                    double average = 0;
+                    int total = 0;
+                    foreach (char character in textBox1.Text)
+                    {
+                        if (char.IsDigit(character))
+                        {
+                            average += char.GetNumericValue(character);
+                            total++;
+                        }
+                    }
+                    if (total > 0)
+                    {
+                        average /= total;
+                    }
+                    label3.Text = average.ToString();
+                    break;
+                case "Кількість чисел":
+                    int count = 0;
+                    foreach (char character in textBox1.Text)
+                    {
+                        if (char.IsDigit(character))
+                        {
+                            count++;
+                        }
+                    }
+                    label3.Text = count.ToString();
+                    break;
+                case "Віддзеркалити значення":
+                    string mirroredValue = string.Empty;
+                    for (int i = textBox1.Text.Length - 1; i >= 0; i--)
+                    {
+                        mirroredValue += textBox1.Text[i];
+                    }
+                    label3.Text = mirroredValue.ToString();
+                    break;
+                case "Змішування":
+                    string mixedValue = string.Empty;
+                    List<string> numbers = new List<string>();
+
+                    for (int i = 0; i < textBox1.Text.Length; i++)
+                    {
+                        numbers.Add(textBox1.Text[i].ToString());
+                    }
+                    Random random = new Random();
+                    while (numbers.Count > 0)
+                    {
+                        int index = random.Next(numbers.Count);
+                        mixedValue += numbers[index];
+                        numbers.RemoveAt(index);
+                    }
+                    label3.Text = mixedValue;
+                    break;
+                default:
+                    label3.Text = "Оберіть операцію";
+                    break;
+            }
+    }
+    private string PerformOperation(string operation, string value)
+        {
+            return "Результат операції";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Сума чисел");
+            comboBox1.Items.Add("Середнє арифметичне");
+            comboBox1.Items.Add("Кількість чисел");
+            comboBox1.Items.Add("Віддзеркалити значення");
+            comboBox1.Items.Add("Змішування");
+            comboBox1.SelectedIndex = 0;
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            {
+                string selectedOperation = comboBox1.SelectedItem.ToString();
+
+                switch (selectedOperation)
+                {
+                    case "Сума чисел":
+                        int sum = 0;
+                        foreach (char digitChar in textBox1.Text)
+                        {
+                            if (char.IsDigit(digitChar))
+                            {
+                                int digit = int.Parse(digitChar.ToString());
+                                sum += digit;
+                            }
+                        }
+                        label3.Text = sum.ToString();
+                        break;
+                    case "Середнє арифметичне":
+                        double average = 0;
+                        int total = 0;
+                        foreach (char character in textBox1.Text)
+                        {
+                            if (char.IsDigit(character))
+                            {
+                                average += char.GetNumericValue(character);
+                                total++;
+                            }
+                        }
+                        if (total > 0)
+                        {
+                            average /= total;
+                        }
+                        label3.Text = average.ToString();
+                        break;
+                    case "Кількість чисел":
+                        int count = 0;
+                        foreach (char character in textBox1.Text)
+                        {
+                            if (char.IsDigit(character))
+                            {
+                                count++;
+                            }
+                        }
+                        label3.Text = count.ToString();
+                        break;
+                    case "Віддзеркалити значення":
+                        string mirroredValue = string.Empty;
+                        for (int i = textBox1.Text.Length - 1; i >= 0; i--)
+                        {
+                            mirroredValue += textBox1.Text[i];
+                        }
+                        label3.Text = mirroredValue.ToString();
+                        break;
+                    case "Змішування":
+                        string mixedValue = string.Empty;
+                        List<string> numbers = new List<string>();
+
+                        for (int i = 0; i < textBox1.Text.Length; i++)
+                        {
+                            numbers.Add(textBox1.Text[i].ToString());
+                        }
+                        Random random = new Random();
+                        while (numbers.Count > 0)
+                        {
+                            int index = random.Next(numbers.Count);
+                            mixedValue += numbers[index];
+                            numbers.RemoveAt(index);
+                        }
+                        label3.Text = mixedValue;
+                        break;
+                    default:
+                        label3.Text = "Помилка операції";
+                        break;
+                }
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Сума чисел");
+            comboBox1.Items.Add("Середнє арифметичне");
+            comboBox1.Items.Add("Кількість чисел");
+            comboBox1.Items.Add("Віддзеркалити значення");
+            comboBox1.Items.Add("Змішування");
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
---
+++ Project5/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Project5
+{
+    public class Class1 : Form
+    {
+        private Label label3;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ComboBox comboBox1;
+
+        public Class1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.label3 = new Label();
+            this.textBox1 = new TextBox();
+            this.button1 = new Button();
+            this.button2 = new Button();
+            this.button3 = new Button();
+            this.comboBox1 = new ComboBox();
+            this.SuspendLayout();
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 0;
+            // textBox1
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // button1
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Почати операцію";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // button2 
+            this.button2.Location = new System.Drawing.Point(118, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // button3
+            this.button3.Location = new System.Drawing.Point(118, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Копіювати";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // Class1
+            this.ClientSize = new System.Drawing.Size(332, 99);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Name = "Class1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+    }
+
+    static class Project5
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Class1());
+        }
+    }
+}
